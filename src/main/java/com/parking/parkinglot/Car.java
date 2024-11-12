daeece686a9e6b487b1dc9de2a550f1103c4cbb3
Razvan961 <razvandragomir50@gmail.com>
2024-11-12 08:49:05 +0200

    parkink lot 6

---
+++ src/main/java/com/parking/parkinglot/Car.java
@@ -0,0 +1,32 @@
+package com.parking.parkinglot;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "car")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
---
+++ src/main/webapp/WEB-INF/pages/menu..jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Visma
+  Date: 11/5/2024
+  Time: 9:08 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+
+</body>
+</html>
